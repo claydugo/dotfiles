fbcc8dfc50111b44d6dd962f0d4878a263760dbd
Clay Dugo <claydugo@gmail.com>
2024-04-04 12:02:37 -0400

    use harpoon2

--- .config/nvim/lua/maps_plugin.lua
+++ .config/nvim/lua/maps_plugin.lua
@@ -4,32 +4,6 @@ vim.keymap.set("n", "<leader>g", ":lua require'telescope.builtin'.live_grep()<CR
 vim.keymap.set("n", "<leader>d", ":lua require'telescope.builtin'.lsp_definitions()<CR>")
 vim.keymap.set("n", "<leader>t", ":lua require'telescope.builtin'.lsp_type_definitions()<CR>")
 
-vim.keymap.set("n", "<leader>hh", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>")
-vim.keymap.set("n", "<leader>ha", ":lua require'harpoon.mark'.add_file()<CR>")
-vim.keymap.set("n", "<leader>1", ":lua require'harpoon.ui'.nav_file(1)<CR>")
-vim.keymap.set("n", "<leader>2", ":lua require'harpoon.ui'.nav_file(2)<CR>")
-vim.keymap.set("n", "<leader>3", ":lua require'harpoon.ui'.nav_file(3)<CR>")
-vim.keymap.set("n", "<leader>4", ":lua require'harpoon.ui'.nav_file(4)<CR>")
-vim.keymap.set("n", "<leader>5", ":lua require'harpoon.ui'.nav_file(5)<CR>")
-vim.keymap.set("n", "<leader>6", ":lua require'harpoon.ui'.nav_file(6)<CR>")
-vim.keymap.set("n", "<leader>7", ":lua require'harpoon.ui'.nav_file(7)<CR>")
-vim.keymap.set("n", "<leader>8", ":lua require'harpoon.ui'.nav_file(8)<CR>")
-vim.keymap.set("n", "<leader>9", ":lua require'harpoon.ui'.nav_file(9)<CR>")
--- not ready still seemingly - 20240325
--- local harpoon = require("harpoon")
--- vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
--- vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
--- vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
--- vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
--- vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
--- vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
--- vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
--- vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
-
 vim.keymap.set("n", "<leader>b", ":lua require'gitsigns'.toggle_current_line_blame()<CR>")
 
 vim.api.nvim_create_augroup("startup", { clear = true })
