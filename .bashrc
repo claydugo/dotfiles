3f93ce3ad394094defafe92cacc3378f3f8dc462
Clay Dugo <claydugo@gmail.com>
2025-05-02 10:22:51 -0400

    mac 2025

--- .bashrc
+++ .bashrc
@@ -95,3 +95,7 @@ fi
 
 source ~/dotfiles/.aliases
 PYGFX_PRINT_WGSL_ON_COMPILATION_ERROR=1
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -56,107 +56,14 @@ ln -sfn ~/dotfiles/.ipython "$XDG_CONFIG_HOME/ipython"
 git config --global user.name "Clay Dugo"
 git config --global user.email "claydugo@gmail.com"
 
-print_message "32" "Installing Kitty terminal..."
-mkdir -p ~/.local/bin/
-mkdir -p ~/.local/share/applications/
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
-ln -sf ~/.local/kitty.app/bin/kitten ~/.local/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=$HOME/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=$HOME/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-
 ln -sf ~/dotfiles/ramona/scripts/ws ~/.local/bin/ws
 ln -sf ~/dotfiles/.local/bin/build_nvim.sh ~/.local/bin/build_nvim
 sudo ln -sf ~/dotfiles/ramona/scripts/drop_caches /usr/sbin/drop_caches
-
-print_message "32" "Installing Starship prompt..."
-curl -sS https://starship.rs/install.sh | sh
-
-cd ~/dotfiles
-git checkout -b "$(hostname)"
 
 os=$(uname -s)
 
 package_manager=""
 common_packages=(neovim fswatch tmux ripgrep htop cmake python3 gnome-tweaks fd-find wget openssl bash)
-
-if [ "$os" == "Linux" ]; then
-    if command -v dnf &> /dev/null; then
-        print_message "31" "Fedora detected."
-        package_manager="dnf"
-        print_message "32" "Installing Fedora packages..."
-        sudo dnf upgrade -y
-        sudo dnf install -y epel-release
-    elif command -v apt-get &> /dev/null; then
-        print_message "31" "Debian detected."
-        package_manager="apt"
-        print_message "32" "Installing Debian packages..."
-        sudo apt-get update
-    else
-        echo "Unsupported Linux distribution."
-        exit 1
-    fi
-elif [ "$os" == "Darwin" ]; then
-    print_message "31" "macOS detected."
-    package_manager="brew"
-    print_message "32" "Installing Homebrew and packages..."
-    if ! command -v brew &> /dev/null; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bashrc
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    else
-        print_message "34" "Homebrew is already installed."
-    fi
-    brew update
-else
-    echo "Unsupported operating system: $os"
-    exit 1
-fi
-
-if [ "$package_manager" == "apt" ] || [ "$package_manager" == "dnf" ]; then
-    install_packages "${common_packages[@]}"
-elif [ "$package_manager" == "brew" ]; then
-    install_packages "${common_packages[@]}"
-    brew install --cask karabiner-elements
-    chsh -s /bin/bash
-    echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bashrc
-fi
-
-if [ "$os" == "Linux" ]; then
-    echo -e "fs.inotify.max_user_watches=100000\nfs.inotify.max_queued_events=100000" | sudo tee -a /etc/sysctl.conf
-    sudo sysctl -p
-    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-    fonts_dir="$HOME/.fonts"
-    mkdir -p "$fonts_dir"
-    cd "$fonts_dir"
-elif [ "$os" == "Darwin" ]; then
-    fonts_dir="/Library/Fonts/"
-    mkdir -p "$fonts_dir"
-    cd "$fonts_dir"
-fi
-
-print_message "32" "Installing Nerd Fonts..."
-if command -v wget &> /dev/null; then
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-elif command -v curl &> /dev/null; then
-    curl -L -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-else
-    echo "Neither wget nor curl is installed. Please install one to proceed."
-    exit 1
-fi
-unzip "FiraCode.zip" -d "$fonts_dir"
-fc-cache -fv
-
-print_message "32" "Installing Conda (Mambaforge)..."
-mkdir -p ~/Downloads
-cd ~/Downloads
-conda_script="Mambaforge-$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m).sh"
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/${conda_script}"
-bash "${conda_script}" -b -p "$HOME/mambaforge"
-echo "export PATH=\"$HOME/mambaforge/bin:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc
 
 print_message "33" "************************************"
 print_message "32" "Setup complete!"
